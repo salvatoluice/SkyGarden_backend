5ecc8400119b88b747ca9668424ff4eeec808178
Salvato Luice <salvatoluice5@gmai.com>
2022-12-19 09:43:36 +0300

    Deployment setup

--- app/controllers/computings_controller.rb
+++ app/controllers/computings_controller.rb
@@ -1,2 +1,12 @@
 class ComputingsController < ApplicationController
+    def index
+        computings = Computing.all
+        render json: computings
+    end
+
+    def show 
+        computings = Computing.find(params[:id])
+        render json: computings
+    end
+
 end
--- db/migrate/20221215171900_create_computings.rb
+++ db/migrate/20221215171900_create_computings.rb
@@ -1,6 +1,11 @@
 class CreateComputings < ActiveRecord::Migration[7.0]
   def change
     create_table :computings do |t|
+      t.string :image
+      t.string :title
+      t.string :seller
+      t.string :description
+      t.decimal :price
 
       t.timestamps
     end
